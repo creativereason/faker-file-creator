ad0b57e224840c3f5b5edb4ea2bb749cc3ef281d
creativereason <brian.schwartz@centricconsulting.com>
2020-05-26 14:45:56 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # faker-file-creator
-Create a file with Faker data
+
+## Create a tab delimited file with Ruby Faker data. 
+
+Wrote this to help create some data I needed for mockups, not much to it. I only created a repo for this because I've done this about 20 times now and always have to look up something and this should speed that up. 
+
+## Requirements
+
+- Ruby
+- [Ruby Faker](https://github.com/faker-ruby/faker) Gem installed
+
+## To Use: 
+
+Start a ruby console
+
+	irb
+
+Run the following commands to generate a file:
+
+	load 'generate.rb'
+	names = GenerateNames.new
+	names.init
+
+If everything is working correctly, this will create a file called __companies.txt__. 
+
+## Next Steps: 
+
+I probably will add to this as I need different data... 
+
+If you have different needs, this is a good example of a Ruby wrapper of Faker for you, it should be pretty easy to create your own or use any of the different methods in Faker.
+
---
+++ generate.rb
@@ -0,0 +1,41 @@
+require 'faker'
+
+class GenerateNames
+  def init
+    row_header = "first_name\tlast_name\tcompany_name\tsales\taddress_1\taddress_2\tcity\tstate\tzip\tphone\tfax\n"
+    file = File.open('companies.txt', 'w+') do |file|
+    file.write(row_header)
+    for i in 0..1000 do
+        a = CompanyContact.new
+        a.first_name = Faker::Name.first_name
+        a.last_name = Faker::Name.last_name
+        a.address_1= Faker::Address.street_address
+        if i % 9 == 0
+          a.address_2 = Faker::Address.secondary_address 
+        end
+        # a.address_2 = Faker::
+        a.city = Faker::Address.city
+        a.state = Faker::Address.state_abbr
+        a.zip = Faker::Address.zip_code
+        a.phone = Faker::PhoneNumber.cell_phone
+        a.fax = Faker::PhoneNumber.phone_number
+        a.company_name = Faker::Company.name
+        file.write(a.to_s)
+      end
+    end
+  end
+end
+
+class CompanyContact
+  attr_accessor :first_name, :last_name, :company_name, :address_1, :address_2, :city, :state, :zip, :phone, :fax
+
+ def sales
+   val = rand(10001..199000)
+   val.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+ end
+
+ def to_s 
+  "#{first_name}\t#{last_name}\t#{company_name}\t$#{sales}\t#{address_1}\t#{address_2}\t#{city}\t#{state}\t#{zip}\t#{phone}\t#{fax}\n"
+ end
+
+end
